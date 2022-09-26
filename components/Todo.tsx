905985764b4af4e354e7e1fa541dbb1c634d9581
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-09-27 03:17:15 +0700

    add+apply: async storage todo

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {useEffect} from 'react';
 import {StyleSheet, ToastAndroid, View} from 'react-native';
-import {Button, Text, TextInput} from 'react-native-paper';
+import {Button, Card, Paragraph, Text, TextInput} from 'react-native-paper';
 
 export const Todo = () => {
   const [todo, setTodo] = React.useState<string[]>([]);
   const [text, setText] = React.useState('');
 
-  function addTodo(name: string) {
-    setTodo([...todo, name]);
+  async function loadTodo() {
+    try {
+      const storageValue = await AsyncStorage.getItem('todo');
+      if (storageValue) {
+        const parsed = JSON.parse(storageValue);
+        setTodo(parsed);
+      }
+    } catch (error) {}
+  }
+
+  async function saveTodo(value: string[]) {
+    await AsyncStorage.setItem('todo', JSON.stringify(value));
+  }
+
+  useEffect(() => {
+    loadTodo();
+  }, []);
+
+  async function addTodo(name: string) {
+    const newTodo = [...todo, name];
+    setTodo(newTodo);
+    saveTodo(newTodo);
+  }
+
+  async function removeTodo(index: number) {
+    ToastAndroid.show(`Removing ${index}`, ToastAndroid.SHORT);
+    let newTodo = [...todo];
+    newTodo = newTodo.filter((__, i) => i !== index);
+    setTodo(newTodo);
+    saveTodo(newTodo);
   }
 
   return (
@@ -30,7 +59,19 @@ export const Todo = () => {
       </Button>
 
       {todo.map((v, i) => (
-        <Text key={i}>{v}</Text>
+        <View key={i}>
+          <Card style={styles.cardStyle}>
+            <Card.Title title={v} />
+            <Card.Content>
+              <Paragraph>
+                #{i} {v}
+              </Paragraph>
+            </Card.Content>
+            <Card.Actions>
+              <Button onPress={() => removeTodo(i)}>Remove</Button>
+            </Card.Actions>
+          </Card>
+        </View>
       ))}
     </View>
   );
@@ -38,4 +79,10 @@ export const Todo = () => {
 
 const styles = StyleSheet.create({
   largeText: {fontSize: 20},
+  cardStyle: {
+    margin: 10,
+    padding: 0,
+    borderWidth: 2,
+    borderColor: 'gray',
+  },
 });
